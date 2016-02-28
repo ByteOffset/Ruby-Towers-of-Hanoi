1060f17572e45e4750863a088ef44f265d1f2f0c
byteoffset <ptown77@gmail.com>
2016-02-27 16:52:13 -0800

    Changed tower labels to letters instead of numbers. I.E. "Tower 1" is now "Tower A". This makes it less confusing for users since the discs are represented as numbers. Also added a loop at the beginning that will prompt the user for the number of discs to use until they enter a number greater than zero.

--- towers_of_hanoi.rb
+++ towers_of_hanoi.rb
@@ -1,6 +1,10 @@
-puts 'How many discs would you like to use?'
-print '>> '
-num_of_discs = gets.to_i
+num_of_discs = 0
+loop do
+	puts 'How many discs would you like to use?'
+	print '>> '
+	num_of_discs = gets.to_i
+	break if num_of_discs > 0
+end
 moves = 0
 initial_disc_array = (1..num_of_discs).to_a.reverse
 towers = {1 => [], 2 => [], 3 => []}
@@ -11,11 +15,13 @@ towers[1] = initial_disc_array
 #towers.each {|tower_number, tower| puts "Tower #{tower_number}: #{tower}"}
 while towers[3].size < num_of_discs
 	puts "\nCurrent status of each tower:"
-	towers.each { |tower_number, tower| puts "Tower #{tower_number}: #{tower}"}
+	# print towers as letters instead of numbers. Do this by adding 64 to the tower number
+	# and converting the resulting integer to its character representation
+	towers.each { |tower_number, tower| puts "Tower #{(tower_number+64).chr}: #{tower}"}
 	puts
 	puts 'Please select a tower from which to take a disc.'
 	print '>> '
-	tower_selected = gets.to_i #expecting integer input
+	tower_selected = gets[0].upcase.ord - 64 #expecting character input. convert to its integer value
 	if tower_selected < 1 || tower_selected > 3
 		puts 'Invalid tower. Please try again.'
 	elsif towers[tower_selected].size < 1
@@ -23,7 +29,7 @@ while towers[3].size < num_of_discs
 	else
 		puts 'Please select a destination for the removed disc.'
 		print '>> '
-		tower_destination = gets.to_i
+		tower_destination = gets[0].upcase.ord - 64
 		if tower_destination < 1 || tower_destination > 3
 			puts 'ERROR: Invalid tower. Please try again.'
 		elsif towers[tower_destination].size > 0 &&
@@ -44,6 +50,7 @@ while towers[3].size < num_of_discs
 	end
 
 end
+
 puts "\n*********************"
 puts 'Well done! You have successfully rearranged the Towers of Hanoi.'
 towers.each { |tower_number, tower| puts "Tower #{tower_number}: #{tower}"}
